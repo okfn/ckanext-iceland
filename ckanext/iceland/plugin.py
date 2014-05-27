d052e7c940f78c6610bd6958a067c3e69b50b5b3
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2014-05-27 13:44:10 +0000

    Add Modernus counter to every page via the base.html template
    The Modernus counter needs three configs in the .ini file:
    
    * iceland.modernus.id (the client's modernus id)
    * iceland.modernus.portion (the portion id for the client)
    * iceland.modernus.name (name of site for the noscript counter)

--- ckanext/iceland/plugin.py
+++ ckanext/iceland/plugin.py
@@ -2,20 +2,42 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
+class ModernusException(Exception):
+    pass
+
+
 class IcelandPlugin(plugins.SingletonPlugin):
     '''Iceland theme plugin.
 
     '''
-    # Declare that this class implements IConfigurer.
+    # Declare that this class implements IConfigurable and IConfigurer.
+    plugins.implements(plugins.IConfigurable)
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
+
+    def configure(self, config):
+        '''Load settings from the config file for this plugin.'''
+
+        # Something
+        if 'iceland.modernus.id' not in config:
+            msg = 'Missing iceland.modernus.id in configuration file'
+            raise ModernusException(msg)
+        self.modernus_id = config['iceland.modernus.id']
+	if 'iceland.modernus.portion' not in config:
+            msg = 'Missing iceland.modernus.portion in configuration file'
+            raise ModernusException(msg)
+        self.modernus_portion = config['iceland.modernus.portion']
+        self.modernus_name = config.get('iceland.modernus.name', '')
+            
 
     def update_config(self, config):
 
-        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # Add this plugin's templates dirs to CKAN's extra_template_paths, so
         # that CKAN will use this plugin's custom templates.
         # 'templates' is the path to the templates dir, relative to this
         # plugin.py file.
         toolkit.add_template_directory(config, 'theme/templates')
+        toolkit.add_template_directory(config, 'modernus/templates')
         
         # Add this plugin's public dir to CKAN's extra_public_paths, so
         # that CKAN will use this plugin's custom static files.
@@ -27,3 +49,15 @@ class IcelandPlugin(plugins.SingletonPlugin):
         # that we'll use to refer to this fanstatic directory from CKAN
         # templates.
         toolkit.add_resource('theme/resources', 'iceland-theme')
+
+
+    def render_modernus(self):
+        data = {'modernus_id': self.modernus_id,
+                'modernus_portion': self.modernus_portion,
+                'modernus_name': self.modernus_name}
+        return toolkit.render_snippet(
+            'modernus/snippets/counter.html',data)
+
+    def get_helpers(self):
+        return {'modernus_counter': self.render_modernus}
+        
